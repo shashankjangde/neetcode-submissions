8659562082b57ecb5f121f1721fc1ffb07d17d1a
Shashank Jangde <shashankjangde2109@gmail.com>
2026-09-08 00:06:19 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = 0, 0
+        while fast<len(nums):
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow==fast:
+                return slow
